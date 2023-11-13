084b3e9089fe5796e5d61fe1221fbd5745e002f1
13101989 <valentin.damache@outlook.com>
2023-11-13 14:48:18 +0200

    fixed pylint issues from tests folder

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,17 @@
+# pylint: disable=C0114
 from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
 
 def test_read_root():
+    """function docstring"""
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hell on World"}
 
 def test_read_data():
+    """function docstring"""
     response = client.get("/data/ion")
     assert response.status_code == 200
     assert response.json() == {"message": "Hell on World Ion"}
